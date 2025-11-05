acb4c86d29ac004794fbe663380f2decf1c917d3
Haebin <nhb0702@naver.com>
2025-11-05 17:53:16 -0500

    unify button components

---
+++ src/components/commons/Button.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function Button({
+  text,
+  title,
+  color = 'default',
+  size = 'medium',
+  onClick,
+  disabled = false,
+  type = 'button',
+  className = '',
+  icon,
+  width,
+  height,
+  style,
+  ...rest
+}) {
+  const buttonClasses = [
+    'btn',
+    `btn-${color}`,
+    `btn-${size}`,
+    className
+  ].filter(Boolean).join(' ');
+
+  const customStyle = {
+    ...(width && { width }),
+    ...(height && { height }),
+    ...style
+  };
+
+  return (
+    <button
+      type={type}
+      className={buttonClasses}
+      onClick={onClick}
+      disabled={disabled}
+      title={title}
+      style={customStyle}
+      {...rest}
+    >
+      {icon && <span className="btn-icon">{icon}</span>}
+      {text}
+    </button>
+  );
+}
+
+export default Button;
